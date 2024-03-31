71269fcf0963c9fdb423b64c958e2b08af4d679d
ramitsuri <ramitsuri007@gmail.com>
2024-03-31 13:48:15 -0400

    Release .2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ramitsuri.podcasts.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = ".2"
     }
     buildFeatures {
         compose = true
