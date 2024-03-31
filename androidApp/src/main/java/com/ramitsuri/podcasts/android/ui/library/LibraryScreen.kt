22f51bfc4ecdaf36beb184ba4351a0bafc3d6080
Ramit Suri <ramitsuri007@gmail.com>
2024-03-30 22:40:55 -0400

    Hide library screens because not ready yet (#48)

--- androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/library/LibraryScreen.kt
+++ androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/library/LibraryScreen.kt
@@ -39,12 +39,15 @@ fun LibraryScreen(
             modifier
                 .fillMaxSize(),
     ) {
-        HorizontalDivider(modifier = Modifier.fillMaxWidth())
-        Item(
-            icon = Icons.Outlined.Subscriptions,
-            labelResId = R.string.subscriptions,
-            onClick = onSubscriptionsClicked,
-        )
+        // TODO enable when ready
+        if (false) {
+            HorizontalDivider(modifier = Modifier.fillMaxWidth())
+            Item(
+                icon = Icons.Outlined.Subscriptions,
+                labelResId = R.string.subscriptions,
+                onClick = onSubscriptionsClicked,
+            )
+        }
         HorizontalDivider(modifier = Modifier.fillMaxWidth())
         Item(
             icon = Icons.AutoMirrored.Default.PlaylistAdd,
@@ -52,18 +55,21 @@ fun LibraryScreen(
             onClick = onQueueClicked,
         )
         HorizontalDivider(modifier = Modifier.fillMaxWidth())
-        Item(
-            icon = Icons.Outlined.ArrowCircleDown,
-            labelResId = R.string.library_downloads,
-            onClick = onDownloadsClicked,
-        )
-        HorizontalDivider(modifier = Modifier.fillMaxWidth())
-        Item(
-            icon = Icons.Default.History,
-            labelResId = R.string.library_history,
-            onClick = onHistoryClicked,
-        )
-        HorizontalDivider(modifier = Modifier.fillMaxWidth())
+        // TODO enable when ready
+        if (false) {
+            Item(
+                icon = Icons.Outlined.ArrowCircleDown,
+                labelResId = R.string.library_downloads,
+                onClick = onDownloadsClicked,
+            )
+            HorizontalDivider(modifier = Modifier.fillMaxWidth())
+            Item(
+                icon = Icons.Default.History,
+                labelResId = R.string.library_history,
+                onClick = onHistoryClicked,
+            )
+            HorizontalDivider(modifier = Modifier.fillMaxWidth())
+        }
     }
 }
 
