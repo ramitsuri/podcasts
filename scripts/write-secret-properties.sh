881f7b8b239acb99947efe588fa3a9bacbe37d01
Ramit Suri <ramitsuri007@gmail.com>
2024-04-17 06:51:22 -0400

    Add release action (#117)

--- scripts/write-secret-properties.sh
+++ scripts/write-secret-properties.sh
@@ -3,6 +3,6 @@
 touch secret.properties
 
 {
-  echo "PODCAST_INDEX_KEY=\"$PODCAST_INDEX_KEY\""
-  echo "PODCAST_INDEX_SECRET=\"$PODCAST_INDEX_SECRET\""
+  echo "PODCAST_INDEX_KEY=$PODCAST_INDEX_KEY"
+  echo "PODCAST_INDEX_SECRET=$PODCAST_INDEX_SECRET"
 } >> secret.properties
