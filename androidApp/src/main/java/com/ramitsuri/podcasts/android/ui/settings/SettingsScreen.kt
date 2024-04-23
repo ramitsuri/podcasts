074d5401373297554826af618b112bfcbcd87992
ramitsuri <ramitsuri007@gmail.com>
2024-04-22 21:25:53 -0400

    Add version, view source to settings

--- androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/settings/SettingsScreen.kt
+++ androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/settings/SettingsScreen.kt
@@ -1,6 +1,7 @@
 package com.ramitsuri.podcasts.android.ui.settings
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -23,6 +25,8 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -37,6 +41,7 @@ import com.ramitsuri.podcasts.utils.Constants
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlin.time.Duration.Companion.minutes
+import com.ramitsuri.podcasts.android.utils.Constants as AppConstants
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,6 +69,8 @@ fun SettingsScreen(
             lastFetchTime = state.lastFetchTime,
             onFetchRequested = onFetchRequested,
         )
+        ColoredHorizontalDivider()
+        AboutApp()
     }
 }
 
@@ -182,6 +189,49 @@ private fun Subtitle(text: String) {
         style = MaterialTheme.typography.bodySmall,
         modifier = Modifier.padding(vertical = 4.dp),
     )
+}
+
+@Composable
+private fun AboutApp() {
+    val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
+    val appVersion = context.packageManager.getPackageInfo(context.packageName, 0)?.versionName ?: ""
+    Column(
+        modifier =
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        CategoryTitle(text = stringResource(id = R.string.settings_about))
+
+        // Version
+        Column(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Title(text = stringResource(id = R.string.settings_version))
+            Subtitle(text = appVersion)
+        }
+
+        // View source
+        Row(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .clickable { uriHandler.openUri(AppConstants.GITHUB_LINK) }
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Title(text = stringResource(id = R.string.settings_view_source))
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.OpenInNew,
+                contentDescription = null,
+            )
+        }
+    }
 }
 
 @ThemePreview
