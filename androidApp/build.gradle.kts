d676e39df589562b79b7147cfe609495ae7476f2
Ramit Suri <ramitsuri007@gmail.com>
2024-05-01 21:13:24 -0400

    Release 1.6

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ramitsuri.podcasts.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.5"
+        versionCode = 16
+        versionName = "1.6"
     }
     buildFeatures {
         compose = true
