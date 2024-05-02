ca23e9610028c2a85ec77e36dc950f0974642b32
ramitsuri <ramitsuri007@gmail.com>
2024-05-02 09:05:27 -0400

    Release 1.7

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ramitsuri.podcasts.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.6"
+        versionCode = 17
+        versionName = "1.7"
     }
     buildFeatures {
         compose = true
