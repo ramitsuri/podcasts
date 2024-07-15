cc89e4dab4a256bd87a6be8eac376c4144b482f8
Ramit Suri <ramitsuri007@gmail.com>
2024-07-15 13:21:19 -0400

    wip

--- shared/src/commonMain/kotlin/com/ramitsuri/podcasts/viewmodel/QueueViewModel.kt
+++ shared/src/commonMain/kotlin/com/ramitsuri/podcasts/viewmodel/QueueViewModel.kt
@@ -1,10 +1,12 @@
 package com.ramitsuri.podcasts.viewmodel
 
+import com.ramitsuri.podcasts.model.Episode
 import com.ramitsuri.podcasts.model.PlayingState
 import com.ramitsuri.podcasts.model.ui.QueueViewState
 import com.ramitsuri.podcasts.repositories.EpisodesRepository
 import com.ramitsuri.podcasts.settings.Settings
 import com.ramitsuri.podcasts.utils.EpisodeController
+import com.ramitsuri.podcasts.utils.QueueRearrangementHelper
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
@@ -19,24 +21,30 @@ class QueueViewModel internal constructor(
     private val _state = MutableStateFlow(QueueViewState())
     val state = _state.asStateFlow()
 
+    private val queueRearrangementHelper = QueueRearrangementHelper(viewModelScope, episodesRepository)
+
     init {
         viewModelScope.launch {
             combine(
+                queueRearrangementHelper.queuePositions,
                 episodesRepository.getQueueFlow(),
                 episodesRepository.getCurrentEpisode(),
                 settings.getPlayingStateFlow(),
-            ) { subscribedEpisodes, currentlyPlayingEpisode, playingState ->
+            ) { queuePositions, subscribedEpisodes, currentlyPlayingEpisode, playingState ->
                 val currentlyPlaying =
                     if (playingState == PlayingState.PLAYING || playingState == PlayingState.LOADING) {
                         currentlyPlayingEpisode
                     } else {
                         null
                     }
-                Triple(subscribedEpisodes, currentlyPlaying, playingState)
-            }.collect { (subscribedEpisodes, currentlyPlayingEpisode, playingState) ->
-                _state.update {
-                    it.copy(
-                        episodes = subscribedEpisodes,
+                Data(queuePositions, subscribedEpisodes, currentlyPlaying, playingState)
+            }.collect { (queuePositions, subscribedEpisodes, currentlyPlayingEpisode, playingState) ->
+                val episodes = subscribedEpisodes.map {
+                    it.copy(queuePosition = queuePositions[it.id] ?: it.queuePosition)
+                }.sortedBy { it.queuePosition }
+                _state.update { viewState ->
+                    viewState.copy(
+                        episodes = episodes,
                         currentlyPlayingEpisodeId = currentlyPlayingEpisode?.id,
                         currentlyPlayingEpisodeState = playingState,
                     )
@@ -49,17 +57,22 @@ class QueueViewModel internal constructor(
         position1: Int,
         position2: Int,
     ) {
-        viewModelScope.launch {
-            val currentlyAtPosition1 = _state.value.episodes.getOrNull(position1)
-            val currentlyAtPosition2 = _state.value.episodes.getOrNull(position2)
-            if (currentlyAtPosition1 != null && currentlyAtPosition2 != null) {
-                episodesRepository.updateQueuePositions(
-                    currentlyAtPosition1.id,
-                    currentlyAtPosition2.queuePosition,
-                    currentlyAtPosition2.id,
-                    currentlyAtPosition1.queuePosition,
-                )
-            }
+        val currentlyAtPosition1 = _state.value.episodes.getOrNull(position1)
+        val currentlyAtPosition2 = _state.value.episodes.getOrNull(position2)
+        if (currentlyAtPosition1 != null && currentlyAtPosition2 != null) {
+            queueRearrangementHelper.updateQueuePositions(
+                currentlyAtPosition1.id,
+                currentlyAtPosition2.queuePosition,
+                currentlyAtPosition2.id,
+                currentlyAtPosition1.queuePosition,
+            )
         }
     }
+
+    private data class Data(
+        val queuePosition: Map<String, Int>,
+        val subscribedEpisodes: List<Episode>,
+        val currentlyPlaying: Episode?,
+        val playingState: PlayingState
+    )
 }
