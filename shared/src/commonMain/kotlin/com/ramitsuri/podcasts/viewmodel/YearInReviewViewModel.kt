cbe0c06662dd26f32b9dfbe2e277b1571f30374d
ramitsuri <ramitsuri007@gmail.com>
2024-12-14 08:25:38 -0500

    wip

---
+++ shared/src/commonMain/kotlin/com/ramitsuri/podcasts/viewmodel/YearInReviewViewModel.kt
@@ -0,0 +1,12 @@
+package com.ramitsuri.podcasts.viewmodel
+
+/*
+most listened to podcasts - get session group by podcastId -> then in code for each group, each entry -> end time - start time
+total hours listened - get all -> end time - start time -> sum
+total episodes listened - get unique podcast+episodeId combinations
+most listened to on days -> get all -> group by local weekday day of week -> one with most hours on average
+most listened to on day -> get all -> group by local day of month of year -> one with most hours
+most listened to on month -> get all -> group by local month of year -> one with most hours
+*/
+class YearInReviewViewModel {
+}
