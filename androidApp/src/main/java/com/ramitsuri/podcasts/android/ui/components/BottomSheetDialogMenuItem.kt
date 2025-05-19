acd4b54e551990f0dc08fbf4e2a5772d30b12fa6
ramitsuri <ramitsuri007@gmail.com>
2025-05-18 20:58:15 -0400

    Add right icon, subtitle to bottom sheet menu item

--- androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/components/BottomSheetDialogMenuItem.kt
+++ androidApp/src/main/java/com/ramitsuri/podcasts/android/ui/components/BottomSheetDialogMenuItem.kt
@@ -1,15 +1,20 @@
 package com.ramitsuri.podcasts.android.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
@@ -18,11 +23,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import com.ramitsuri.podcasts.android.ui.PreviewTheme
+import com.ramitsuri.podcasts.android.ui.ThemePreview
 
 @Composable
 fun BottomSheetDialogMenuItem(
-    icon: ImageVector? = null,
+    startIcon: ImageVector? = null,
+    endIcon: ImageVector? = null,
     text: String,
+    subtitle: String? = null,
     switchState: SwitchState? = null,
     onClick: () -> Unit,
 ) {
@@ -35,11 +44,17 @@ fun BottomSheetDialogMenuItem(
                         .padding(vertical = 16.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                icon?.let {
+                startIcon?.let {
                     Icon(imageVector = it, contentDescription = null)
                     Spacer(modifier = Modifier.width(16.dp))
                 }
-                Text(text)
+                Column {
+                    Text(text)
+                    subtitle?.let {
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = it, style = MaterialTheme.typography.bodySmall)
+                    }
+                }
                 Spacer(modifier = Modifier.weight(1f))
                 switchState?.let {
                     Switch(
@@ -59,6 +74,9 @@ fun BottomSheetDialogMenuItem(
                             },
                     )
                 }
+                endIcon?.let {
+                    Icon(imageVector = it, contentDescription = null)
+                }
             }
         },
         onClick = onClick,
@@ -68,3 +86,45 @@ fun BottomSheetDialogMenuItem(
 data class SwitchState(
     val checked: Boolean,
 )
+
+@ThemePreview
+@Composable
+private fun MenuItem() {
+    PreviewTheme {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            BottomSheetDialogMenuItem(
+                startIcon = Icons.Filled.Check,
+                text = "Text",
+                subtitle = "Subtitle",
+                switchState = SwitchState(checked = true),
+                onClick = {},
+            )
+            BottomSheetDialogMenuItem(
+                startIcon = Icons.Filled.Check,
+                text = "Text",
+                switchState = SwitchState(checked = true),
+                onClick = {},
+            )
+            BottomSheetDialogMenuItem(
+                text = "Text",
+                subtitle = "Subtitle",
+                switchState = SwitchState(checked = true),
+                onClick = {},
+            )
+            BottomSheetDialogMenuItem(
+                text = "Text",
+                subtitle = "Subtitle",
+                endIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                onClick = {},
+            )
+            BottomSheetDialogMenuItem(
+                text = "Text",
+                endIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                onClick = {},
+            )
+        }
+    }
+}
