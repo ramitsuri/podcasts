1fc0445caecb3283829817e7e302b82ca2c76c36
ramitsuri <ramitsuri007@gmail.com>
2025-12-05 08:10:20 -0500

    Release 5.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ramitsuri.podcasts.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 49
-        versionName = "4.9"
+        versionCode = 50
+        versionName = "5.0"
     }
     buildFeatures {
         buildConfig = true
